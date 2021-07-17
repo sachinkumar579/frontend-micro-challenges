b467e4e4140f1c2b182b3c870ee867d9aa3b7fad
sachin kumar <ssachin579@gmail.com>
2021-07-18 00:01:39 +0530

    refactoring

--- star-rating/index.js
+++ star-rating/index.js
@@ -1,65 +1,53 @@
 window.onload = function () {
   let clicked = []; // To store the button click state. This will be cleared on browser refresh
-  const totalStars = 5; // Total stars to be displayed in the UI
+  const starCount = 5; // Total stars to be displayed in the UI
 
   //This will be the main container
   let div = document.getElementById("div");
 
   // Create 1 div for each star and add it to main div
-  for (let i = 1; i <= totalStars; i++) {
-    let innerDiv = document.createElement("div");
-    innerDiv.className = "white";
-    div.appendChild(innerDiv);
+  for (let i = 1; i <= starCount; i++) {
+    let starDiv = document.createElement("div");
+    starDiv.id = i;
+    starDiv.className = "white";
+    div.appendChild(starDiv);
   }
 
   // Iterate through each star div and add 3 listeners - mouseover,mouseout and click
-  for (let i = 1; i <= div.childElementCount; i++) {
-    if (div.childNodes[i].nodeName == "DIV") {
-      mouseOverListener(div, i);
+  for (let activeElement of div.children) {
+    activeElement.addEventListener("mouseover", updateState);
+    activeElement.addEventListener("mouseout", updateState);
+    activeElement.addEventListener("click", updateState);
 
-      mouseOutListener(div, i);
+    function updateState(event) {
+      if (event.type == "click") clicked = [];
 
-      mouseClickListener(div, i);
+      for (let element of div.children) {
+        toggleImages(
+          event,
+          element,
+          parseInt(element.id),
+          parseInt(activeElement.id)
+        );
+      }
     }
   }
 
-  // Logic to toggle between yellow and white image
-  function mouseOverListener(div, i) {
-    div.childNodes[i].addEventListener("mouseover", function () {
-      for (let j = div.childElementCount; j > 0; j--) {
-        if (j > i) {
-          div.childNodes[j].className = "white";
-        } else {
-          div.childNodes[j].className = "yellow";
-        }
-      }
-    });
-  }
-
-  function mouseOutListener(div, i) {
-    div.childNodes[i].addEventListener("mouseout", function () {
-      for (let j = div.childElementCount; j > 0; j--) {
-        if (!clicked.includes(j)) {
-          div.childNodes[j].className = "white";
-        } else {
-          div.childNodes[j].className = "yellow";
-        }
-      }
-    });
-  }
-
-  function mouseClickListener(div, i) {
-    div.childNodes[i].addEventListener("click", function () {
-      clicked = [];
-      for (let j = i; j > 0; j--) {
-        clicked.push(j);
-        div.childNodes[j].className = "yellow";
-      }
-      for (let j = div.childElementCount; j > 0; j--) {
-        if (!clicked.includes(j)) {
-          div.childNodes[j].className = "white";
-        }
-      }
-    });
+  function toggleImages(event, element, elementId, activeElementId) {
+    switch (event.type) {
+      case "mouseover":
+        elementId <= activeElementId
+          ? (element.className = "yellow")
+          : (element.className = "white");
+        break;
+      case "mouseout":
+        clicked.includes(elementId)
+          ? (element.className = "yellow")
+          : (element.className = "white");
+        break;
+      case "click":
+        elementId <= activeElementId ? clicked.push(elementId) : null;
+        break;
+    }
   }
 };
