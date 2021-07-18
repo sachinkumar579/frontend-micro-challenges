71d2d623beca42924ab8f79dfb2298883ffd81b0
sachinkumar <32408232+sachinkumar579@users.noreply.github.com>
2021-07-18 23:12:40 +0530

    another version

--- star-rating/index.js
+++ star-rating/index.js
@@ -1,45 +1,34 @@
 window.onload = function () {
-  let clicked = []; // To store the button click state. This will be cleared on browser refresh
-  const starCount = 5; // Total stars to be displayed in the UI
-
-  //This will be the main container
-  let div = document.getElementById("div");
-
-  // Create 1 div for each star and add it to main div
-  for (let i = 1; i <= starCount; i++) {
-    let starDiv = document.createElement("div");
-    starDiv.id = i;
-    starDiv.className = "white";
-    div.appendChild(starDiv);
-  }
-
-  // Iterate through each star div and add 3 listeners - mouseover,mouseout and click
-  for (let activeElement of div.children) {
-    activeElement.addEventListener("mouseover", updateState);
-    activeElement.addEventListener("mouseout", updateState);
-    activeElement.addEventListener("click", updateState);
-
+    let clicked = []; // To store the button click state. This will be cleared on browser refresh
+    const starCount = 10; // Total stars to be displayed in the UI
+    const actions = ["mouseover", "mouseout", "click"];
+  
+    let div = document.getElementById("div");  //This will be the main container
+  
+   for (let i = 1; i <= starCount; i++) {
+      let starDiv = document.createElement("div"); // Create 1 div for each star and add it to main div
+      starDiv.id = i;
+      starDiv.className = "white";
+      actions.forEach((action) => {
+        starDiv.addEventListener(action, updateState);  // Add 3 listeners - mouseover,mouseout and click  
+      });
+      div.appendChild(starDiv);
+    }
+  
     function updateState(event) {
       if (event.type == "click") clicked = [];
-
       for (let element of div.children) {
-        toggleImages(event, element, parseInt(element.id), parseInt(activeElement.id));
+        let elementId = parseInt(element.id);
+        let activeElementId = parseInt(event.currentTarget.id);
+        switch (event.type) {
+          case "mouseover":
+            elementId <= activeElementId ? (element.className = "yellow") : (element.className = "white");
+            break;
+          case "mouseout": clicked.includes(elementId) ? (element.className = "yellow") : (element.className = "white");
+            break;
+          case "click": elementId <= activeElementId ? clicked.push(elementId) : null;
+            break;
+        }
       }
     }
-  }
-
-  function toggleImages(event, element, elementId, activeElementId) {
-    switch (event.type) {
-      case "mouseover":
-        elementId <= activeElementId ? (element.className = "yellow") : (element.className = "white");
-        break;
-      case "mouseout":
-        clicked.includes(elementId) ? (element.className = "yellow") : (element.className = "white");
-        break;
-      case "click":
-        elementId <= activeElementId ? clicked.push(elementId) : null;
-        break;
-    }
-  }
-  
-};
+  };
