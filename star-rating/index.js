3356c51b1f055a069534b156de3bfdc6984a9ab3
sachinkumar <32408232+sachinkumar579@users.noreply.github.com>
2021-07-23 01:00:51 +0530

    refactoring

--- star-rating/index.js
+++ star-rating/index.js
@@ -2,22 +2,38 @@ window.onload = function () {
   let clicked = []; // To store the button click state. This will be cleared on browser refresh
   const starCount = 5; // Total stars to be displayed in the UI
   const actions = ["mouseover", "mouseout", "click"];
+  const div = document.getElementById("div"); //This will be the container for stars
+  const emojiDiv = document.getElementById("emojiDiv"); // Div for emojis
 
-  const div = document.getElementById("div"); //This will be the main container
+  // Clear everything in the UI
+  for (let emojiChild of emojiDiv.children) {
+    emojiChild.style.display = "none";
+  }
 
+  // Create 1 div for each star and add it to main div
+  // Add 3 listeners - mouseover,mouseout and click for each star
   for (let i = 1; i <= starCount; i++) {
-    const starDiv = document.createElement("div"); // Create 1 div for each star and add it to main div
+    const starDiv = document.createElement("div"); 
     starDiv.id = i;
     starDiv.className = "white";
     actions.forEach((action) => {
-      starDiv.addEventListener(action, updateState); // Add 3 listeners - mouseover,mouseout and click
+      starDiv.addEventListener(action, updateState); 
     });
     div.appendChild(starDiv);
   }
 
   function updateState(event) {
     const activeElementId = Number(event.currentTarget.id);
-    if (event.type == "click") clicked = [];
+
+    if (event.type == "click") {
+      clicked = [];
+      for (let emojiChild of emojiDiv.children) {
+        emojiChild.id == "star" + activeElementId
+          ? (emojiChild.style.display = "block")
+          : (emojiChild.style.display = "none");
+      }
+    }
+
     for (let element of div.children) {
       const elementId = Number(element.id);
       switch (event.type) {
