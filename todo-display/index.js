6a47a6012724b68e8818672bc595d1f2b0c80cab
sachinkumar <32408232+sachinkumar579@users.noreply.github.com>
2021-07-25 16:56:58 +0530

    refactoring

--- todo-display/index.js
+++ todo-display/index.js
@@ -10,15 +10,15 @@ window.onload = function () {
     "TypeScript",
   ];
 
-  let listContainer = document.getElementById("listContainer");
+  const listContainer = document.getElementById("listContainer");
 
   // Iterating over all TODO list items to create a text node and button
   // within a div. An event listener is attached to the button which
   // will remove the individual div from the main div
   for (let count in todoItems) {
-    let todo = createDiv();
-    let todoItemName = document.createTextNode(todoItems[count]);
-    let button = createButton(todo);
+    const todo = createDiv();
+    const todoItemName = document.createTextNode(todoItems[count]);
+    const button = createButton(todo);
 
     todo.appendChild(todoItemName);
     todo.appendChild(button);
@@ -26,13 +26,13 @@ window.onload = function () {
   }
 
   function createDiv() {
-    let todo = document.createElement("div");
+    const todo = document.createElement("div");
     todo.className = "todo-item";
     return todo;
   }
 
   function createButton(todo) {
-    let button = document.createElement("button");
+    const button = document.createElement("button");
     button.className = "button";
     button.textContent = "Done";
 
