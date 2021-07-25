94d79c08f694ae87744f3a81f695917c19a3d705
sachin kumar <ssachin579@gmail.com>
2021-07-25 20:23:57 +0530

    refactoring- moving css styling to style.css

--- star-rating/index.js
+++ star-rating/index.js
@@ -1,59 +1,49 @@
-window.onload = function () {
-  let clicked = []; // To store the button click state. This will be cleared on browser refresh
-  const starCount = 10; // Total stars to be displayed in the UI
-  const actions = ["mouseover", "mouseout", "click"];
-  const starContainer = document.getElementById("starContainer");
-  const emojiContainer = document.getElementById("emojiContainer");
-  const emoji = document.getElementById("emoji"); // to display one of the below emojis
-  const ratings = ["😥", "😌", "😐", "😃", "😎"];
+let clicked = []; // To store the button click state. This will be cleared on browser refresh
+const starCount = 5; // Total stars to be displayed in the UI
+const actions = ["mouseover", "mouseout", "click"];
+const starContainer = document.getElementById("starContainer");
+const emojiContainer = document.getElementById("emojiContainer");
+const emoji = document.getElementById("emoji"); // to display one of the below emojis
+const ratings = ["😥", "😌", "😐", "😃", "😎"];
 
-  // Create 1 span for each star and add it to main div
-  // Add 3 listeners - mouseover,mouseout and click for each star
-  for (let i = 1; i <= starCount; i++) {
-    const starSpan = document.createElement("span");
-    starSpan.id = i;
-    starSpan.textContent = "☆";
-    starSpan.style.color = "black";
-    starSpan.style.cursor = "pointer";
-    starSpan.className = "star";
+// Create 1 span for each star and add it to main div
+// Add 3 listeners - mouseover,mouseout and click for each star
+for (let i = 1; i <= starCount; i++) {
+  const star = document.createElement("span");
+  star.id = i;
+  star.className = "star-white";
 
-    actions.forEach((action) => {
-      starSpan.addEventListener(action, updateState);
-    });
-    starContainer.appendChild(starSpan);
+  actions.forEach((action) => {
+    star.addEventListener(action, updateState);
+  });
+  starContainer.appendChild(star);
+}
+
+function updateState(event) {
+  const activeElementId = Number(event.currentTarget.id);
+
+  if (event.type == "click") {
+    clicked = [];
+    emoji.textContent =
+      ratings[Math.ceil((activeElementId * 5) / starCount - 1)]; // rounds a number to next largest integer
   }
 
-  function updateState(event) {
-    const activeElementId = Number(event.currentTarget.id);
-
-    if (event.type == "click") {
-      clicked = [];
-      emoji.textContent =
-        ratings[Math.ceil((activeElementId * 5) / starCount - 1)]; // rounds a number to next largest integer
-    }
-
-    for (let starElement of starContainer.children) {
-      const elementId = Number(starElement.id);
-
-      if (event.type == "mouseover") {
-        if (elementId <= activeElementId) {
-          starElement.textContent = "★";
-          starElement.style.color = "yellow";
-        } else {
-          starElement.textContent = "☆";
-          starElement.style.color = "black";
-        }
-      } else if (event.type == "mouseout") {
-        if (clicked.includes(elementId)) {
-          starElement.textContent = "★";
-          starElement.style.color = "yellow";
-        } else {
-          starElement.textContent = "☆";
-          starElement.style.color = "black";
-        }
-      } else if (event.type == "click") {
+  for (let starElement of starContainer.children) {
+    const elementId = Number(starElement.id);
+    switch (event.type) {
+      case "mouseover":
+        elementId <= activeElementId
+          ? (starElement.className = "star-yellow")
+          : (starElement.className = "star-white");
+        break;
+      case "mouseout":
+        clicked.includes(elementId)
+          ? (starElement.className = "star-yellow")
+          : (starElement.className = "star-white");
+        break;
+      case "click":
         elementId <= activeElementId ? clicked.push(elementId) : null;
-      }
+        break;
     }
   }
-};
+}
